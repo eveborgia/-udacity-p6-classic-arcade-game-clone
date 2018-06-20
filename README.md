c870e799c9f6e89287a70cd5260a883f84294769
Evelina Borgia <evelinaborgia@gmail.com>
2018-06-20 01:28:58 -0400

    working version

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Instructions on how to:
+1. Run 
+The repository contains css, images, and js folders, as well as an index.html and a README.md file.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+The css folder contains a style.css file.
+The images folder contains the png image files, which are used when displaying the game. The images for the player and enemy character are going to be loaded from this folder.
+The js folder also contains the app engine needed to run the game and a resources.js file.
+index.html - opening index.html should loads the game.
+README.md contains instructions on how to load and play the game.
+
+Inside the app.js file, is implemented the Player and the Enemy classes, using Object-Oriented JavaScript. 
+
+The Enemy function, which initiates the Enemy by:
+Loading the image by setting this.sprite to the appropriate image in the image folder.
+Setting the Enemy initial location.
+Setting the Enemy speed.
+The update method for the Enemy.
+Updates the Enemy location.
+Handles collision with the Player.
+
+The Player function, which initiates the Player by:
+Loading the image by setting this.sprite to the appropriate image in the image folder .
+Setting the Player initial location.
+The update method for the Player .
+The render method for the Player.
+The handleInput method, which is receiving user input, allowedKeys and moves the player according to that input. In particular:
+Left key moves the player to the left, right key to the right, up moves the player up and down moves the player down.
+The player cannot move off screen.
+If the player reaches the water the game resets by moving the player back to the initial location.
+
+2. Play the arcade game.
+
+In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
--- js/app.js
+++ js/app.js
@@ -1,61 +1,95 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+const Enemy = function(x, y, speed) {
+    // Variables applied to each of the instances
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image/sprite for our Enemies
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//The update method for the Enemy
+// Setting the Enemy speed
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += this.speed * dt;
+
+    //If Enemy goes outside the canvas reapears on the left 
+    if (this.x > 505) {
+        this.x = -30;
+        this.speed = 50 + Math.floor(Math.random() * 250)
+    }
+
+    //Length of Player and sprite for colision
+    // Send the Player to the initial location
+    if (player.x < this.x + 90 &&
+    player.x + 90 > this.x &&
+    player.y < this.y + 60 &&
+    60 + player.y > this.y) {
+        player.x = 203;
+        player.y = 380;
+    }
 };
 
-// Draw the enemy on the screen, required method for game
+// The render method for the Enemy
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-var Player = function() {
-    this.sprite = 'images/enemy-bug.png';
+//Loading the image of the Player
+const Player = function(x, y) {
+    this.player = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
 };
 
+//The update method for the Player
 Player.prototype.update = function(dt) {
-
 };
 
+//The render method for the Player
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
 };
 
+//Receives user input and moves the Player according
+Player.prototype.handleInput = function(keyPress){
+    if( keyPress == 'left' && this.x > 50 ) {
+        this.x -= 101;
+    } else if( keyPress == 'right' && this.x < 380) {
+        this.x += 101;
+    } else if(keyPress == 'down' && this.y < 380) {
+        this.y += 80;
+    } else if(keyPress == 'up' && this.y > 50) {
+        this.y -= 80;
+    }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    //When Player reaches water the game resets by moving the Player back to initial location
+    if (this.y < 101) {
+        setTimeout(function () {
+            player.x = 203;
+            player.y = 380;
+        }, 500);
+    }
+};
+
+// All Enemy objects in an array called allEnemies
 var allEnemies = [];
 
-var bug1 = new Enemy();
-var bug2 = new Enemy();
-var bug3 = new Enemy();
-var bug4 = new Enemy();
-var bug5 = new Enemy();
+// Setting the Enemy initial location.
+var enemyLocation = [58, 140, 225]
 
-allEnemies[bug1, bug2, bug3, bug4, bug5];
+//Enemy start and the speed when reaches the end of canvas
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(50, locationY, 250);
+    allEnemies.push(enemy); 
+});
 
-var player = new Player();
+// Place the Player object in a variable called Player and set the Player initial location
+var player = new Player(203, 380);
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the Player to desired location
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
